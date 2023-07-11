e7ee5a21c59886325297a7bd29a17194e57c7dc4
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2023-07-11 00:44:35 -0600

    updated maze to prim's maze

--- Path Finding Algorithm.py
+++ Path Finding Algorithm.py
@@ -141,21 +141,47 @@ def get_clicked_pos(pos, rows, width):
     return row, col
 
 
-def generate_maze(grid, density=0.3):
+def generate_maze_prim(grid, start):
+    dx = [0, 1, 0, -1]
+    dy = [-1, 0, 1, 0]
+
+    # Initially mark all cells as walls
     for row in grid:
         for node in row:
-            if random.random() < density:
-                node.make_barrier()
+            node.make_barrier()
+
+    # Start with a grid full of walls
+    x, y = start
+    grid[x][y].reset()
+
+    wall_list = [(x + dx[i], y + dy[i], i) for i in range(4)]
+    while wall_list:
+        random.shuffle(wall_list)
+        wx, wy, wi = wall_list.pop()
+
+        px, py = wx + dx[wi], wy + dy[wi]
+
+        if px >= 0 and py >= 0 and px < len(grid) and py < len(grid[0]) and grid[px][py].is_barrier():
+            grid[wx][wy].reset()
+            grid[px][py].reset()
+
+            for i in range(4):
+                nx, ny = px + dx[i], py + dy[i]
+                if nx >= 0 and ny >= 0 and nx < len(grid) and ny < len(grid[0]) and grid[nx][ny].is_barrier():
+                    wall_list.append((nx, ny, i))
 
 
 def run_search(algorithm, width):
     ROWS = 50
     grid = make_grid(ROWS, width)
-    generate_maze(grid, density=0.3)
     start = random.choice(random.choice(grid))
-    end = random.choice(random.choice(grid))
-    while start == end or start.is_barrier() or end.is_barrier():
+    while start.is_barrier():
         start = random.choice(random.choice(grid))
+
+    generate_maze_prim(grid, (start.row, start.col))
+
+    end = random.choice(random.choice(grid))
+    while end == start or end.is_barrier():
         end = random.choice(random.choice(grid))
 
     start.make_start()
