63aef16b063d98784dfc9799dbcc6c0f1ea162ce
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2023-07-12 15:31:24 -0600

    still need to fix the rest button

--- main.py
+++ main.py
@@ -7,60 +7,101 @@ from maze import generate_maze_prim
 from algorithms import astar, bfs, dfs, gbfs, dijkstra
 
 
+# Create a pygame display
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+
+# Get the width and height of the display
+win_width, win_height = WIN.get_size()
+reset_button = Button(RED, YELLOW, win_width // 2 - 100,
+                      (win_height // 2 + 240) - (win_height - 200), 200, 60, text='RESET')
+main_menu_button = Button(BLUE, YELLOW, win_width // 2 - 100,
+                          (win_height // 2 + 310) - (win_height - 200), 200, 60, text='MAIN')
+
+
 def run_search(algorithm, width):
     ROWS = 50
     grid = make_grid(ROWS, width)
-    start = random.choice(random.choice(grid))
-    while start.is_barrier():
-        start = random.choice(random.choice(grid))
 
-    generate_maze_prim(grid, (start.row, start.col))
+    # Generate maze before user starts picking points
+    generate_maze_prim(grid, (ROWS//2, ROWS//2))
+    start = None
+    end = None
+    running = True
+    while running:
+        draw(WIN, grid, ROWS, width)
+        pygame.display.update()
 
-    end = random.choice(random.choice(grid))
-    while end == start or end.is_barrier():
-        end = random.choice(random.choice(grid))
-
-    start.make_start()
-    end.make_end()
-
-    for row in grid:
-        for node in row:
-            node.update_neighbors(grid)
-
-    success = algorithm(lambda: draw(WIN, grid, ROWS, width), grid, start, end)
-
-    if success:
-        reset_button = Button(RED, YELLOW, 650, 680, 150, 50, text='RESET')
-        main_menu_button = Button(BLUE, YELLOW, 650, 740, 150, 50, text='MAIN')
-
-        running = True
-        while running:
-            for event in pygame.event.get():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button
                 pos = pygame.mouse.get_pos()
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    return
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                if not start:
+                    start = node
+                    start.make_start()
+                elif not end and node != start:
+                    end = node
+                    end.make_end()
+                elif node != end and node != start:
+                    node.make_barrier()
+            elif pygame.mouse.get_pressed()[2]:  # Right mouse button
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
 
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if reset_button.is_over(pos):
-                        print('RESET clicked')
-                        # Here, instead of returning to the main, we can call run_search again with the same algorithm
-                        run_search(algorithm, width)
-                        running = False
+        if start and end:
+            for row in grid:
+                for node in row:
+                    node.update_neighbors(grid)
+            success = algorithm(lambda: draw(
+                WIN, grid, ROWS, width), grid, start, end)
+            if success:
+                break  # Break the loop if algorithm finished successfully
 
-                    if main_menu_button.is_over(pos):
-                        print('Main Menu clicked')
-                        # This would take the user back to the main function
-                        main(WIN, width)
-                        running = False
+    # Create a surface for the buttons
+    button_surface = pygame.Surface((win_width, 200), pygame.SRCALPHA)  # Note the pygame.SRCALPHA flag
+    button_surface.fill((255, 255, 255, 0))  # Fill it with a transparent color
 
-            # Draw buttons
-            reset_button.draw(WIN, BLACK)
-            main_menu_button.draw(WIN, BLACK)
-            pygame.display.update()
+    # Draw the grid once before starting the loop
+    draw(WIN, grid, ROWS, width)
+    
+    # New loop to keep handling events
+    while success:
+        button_surface.fill((255, 255, 255, 0))  # Clear the button surface
+        reset_button.draw(button_surface)  # Draw the buttons on the button surface
+        main_menu_button.draw(button_surface)
+        WIN.blit(button_surface, (0, win_height - 200))  # Blit the button surface onto the main window
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                success = False
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button
+                pos = pygame.mouse.get_pos()
+                pos_adjusted = (pos[0], pos[1] - (win_height - 200))  # Adjust the mouse position
+                if reset_button.is_over(pos_adjusted):
+                    print("Reset button clicked")
+                    # Add code to reset the game here
+                    success = False  # Break the loop if reset button is clicked
+                elif main_menu_button.is_over(pos_adjusted):
+                    print("Main Menu button clicked")
+                    # Add code to go to the main menu here
+                    success = False  # Break the loop if main menu button is clicked
+
+    return success  # Return the status
 
 
 def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
     # Create buttons with normal and selected colors
     buttons = {
         'bfs': Button(GREEN, YELLOW, win_width // 2 - 100, win_height // 2 - 180, 200, 60, text='BFS'),
@@ -72,65 +113,73 @@ def main(win, width):
     start_button = Button(ORANGE, YELLOW, win_width // 2 -
                           100, win_height // 2 + 170, 200, 60, text='Start')
 
-    font = pygame.font.SysFont('arial', 30)
-    text_surface = font.render(
-        'Choose an algorithm, then press Start', True, BLACK)
-    text_width, text_height = text_surface.get_size()
-
     instructions = Text('Choose an algorithm, then press Start',
-                        win_width // 2 - text_width // 2,
-                        win_height // 2 - 250)
+                        win_width // 2 - 75, win_height // 2 - 250)
 
     selected_algorithm = None  # Variable to store the selected algorithm
     selected_button = None  # Variable to store the selected button
+    algorithm_finished = False  # Variable to store whether the algorithm has finished
 
     running = True
     while running:
-        # Event handling
-        for event in pygame.event.get():
-            pos = pygame.mouse.get_pos()
-
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-
-                if buttons['bfs'].is_over(pos):
-                    print('BFS selected')
-                    selected_algorithm = bfs
-                    selected_button = 'bfs'
-                elif buttons['dfs'].is_over(pos):
-                    print('DFS selected')
-                    selected_algorithm = dfs
-                    selected_button = 'dfs'
-                elif buttons['gbfs'].is_over(pos):
-                    print('Greedy-BFS selected')
-                    selected_algorithm = gbfs
-                    selected_button = 'gbfs'
-                elif buttons['astar'].is_over(pos):
-                    print('A* selected')
-                    selected_algorithm = astar
-                    selected_button = 'astar'
-                elif buttons['dijkstra'].is_over(pos):
-                    print('Dijkstra selected')
-                    selected_algorithm = dijkstra
-                    selected_button = 'dijkstra'
-                elif start_button.is_over(pos) and selected_algorithm is not None:
-                    print('Start button clicked')
-                    run_search(selected_algorithm, width)
-
-                # Reset all buttons to not selected
-                for button in buttons.values():
-                    button.selected = False
-                # Set the clicked button to selected
-                if selected_button:
-                    buttons[selected_button].selected = True
-
-        # Redraw the window
         win.fill(WHITE)
         for button in buttons.values():
-            button.draw(win, BLACK)
-        start_button.draw(win, BLACK)
+            button.draw(win)
+        start_button.draw(win)
         instructions.draw(win)
+        if algorithm_finished:
+            reset_button.draw(win)
+            main_menu_button.draw(win)
+        pygame.display.update()
+
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button
+                pos = pygame.mouse.get_pos()
+                if start_button.is_over(pos) and selected_algorithm is not None and not algorithm_finished:
+                    print("Start button clicked")
+                    # Update the status based on the return value of run_search
+                    algorithm_finished = run_search(selected_algorithm, width)
+                elif reset_button.is_over(pos) and algorithm_finished:
+                    print("Reset button clicked")
+                    # Add code to reset the game here
+                    algorithm_finished = False  # Reset the status
+                elif main_menu_button.is_over(pos) and algorithm_finished:
+                    print("Main Menu button clicked")
+                    # Add code to go to the main menu here
+                    algorithm_finished = False  # Reset the status
+                else:
+                    # Check which algorithm button was clicked
+                    if buttons['bfs'].is_over(pos):
+                        print("BFS selected")
+                        selected_algorithm = bfs
+                        selected_button = 'bfs'
+                    elif buttons['dfs'].is_over(pos):
+                        print("DFS selected")
+                        selected_algorithm = dfs
+                        selected_button = 'dfs'
+                    elif buttons['gbfs'].is_over(pos):
+                        print("Greedy-BFS selected")
+                        selected_algorithm = gbfs
+                        selected_button = 'gbfs'
+                    elif buttons['astar'].is_over(pos):
+                        print("A* selected")
+                        selected_algorithm = astar
+                        selected_button = 'astar'
+                    elif buttons['dijkstra'].is_over(pos):
+                        print("Dijkstra selected")
+                        selected_algorithm = dijkstra
+                        selected_button = 'dijkstra'
+
+                    # Reset all buttons to not selected
+                    for button in buttons.values():
+                        button.selected = False
+
+                    # Set the clicked button to selected
+                    if selected_button:
+                        buttons[selected_button].selected = True
 
         pygame.display.update()
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-    end = random.choice(random.choice(grid))
-    while end == start or end.is_barrier():
-        end = random.choice(random.choice(grid))
+        draw(WIN, grid, ROWS, width)
